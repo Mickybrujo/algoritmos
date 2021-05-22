5035e6a6be40ba1da49165a4f29807b52a7765af
Piripituchi <pecus9@hotmail.com>
2021-05-22 02:04:29 -0500

    creacion de lista y ordenamiento

--- Practica3/huffman_coder.c
+++ Practica3/huffman_coder.c
@@ -1,3 +1,26 @@
+//*****************************************************************
+//HUFFMAN_CODER.C
+//*****************************************************************
+//*****************************************************************
+//Integrantes: Amazul
+/*
+    2019630068 ANGELES HERNANDEZ JESUS EDUARDO 
+    2015060081 BAEZA COLIN ANA KAREN
+    2019630052 BECERRA RAMIREZ LUIS ARTURO 
+    2018630131 BUSTAMANTE MENDOZA MIGUEL IMANOL
+    */
+//Curso: Análisis de algoritmos
+//Grupo: 3CM12
+//Carrera: Ingenieria en Sistemas Computacionales (Plan 2009)
+//Mayo 2021
+//ESCOM-IPN
+//Descripcion:
+/*
+    Codifica un fichero utilizando el algoritmo de Huffman.
+*/
+//Compilación de la libreria: "gcc huffman_coder.c" (Generación del ejecutable)
+//*****************************************************************
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -9,45 +32,79 @@ struct tree
     struct tree *izq, *der;
 };
 
-
-int *readTxt(int cant_datos, FILE *datos) /*editar*/
+struct list
 {
-    int *array_datos;
-    array_datos = (int *)malloc(sizeof(int) * cant_datos); //Reservamos memoria suficiente para la cantidad de datos
-    for (int i = 0; i < cant_datos; i++)                   //Recorremos los datos y los guardamos en el arreglo
+    struct tree *nodo;
+    struct list *next;
+};
+
+void insertarLista(struct list **lista, int caracter, int dato){
+    struct list *nuevo=NULL;
+    struct list *inicio=(*lista);
+    nuevo=(struct list *)malloc(sizeof(struct list));
+    nuevo->nodo=(struct tree *)malloc(sizeof(struct tree ));
+    nuevo->nodo->repeticiones=dato;
+    if (caracter!=-1)
     {
-        fscanf(datos, "%d", &array_datos[i]);
+        nuevo->nodo->caracter = (char) caracter;
     }
-    return array_datos; //Regresamos el arreglo con los datos
+    if (!(*lista)||dato<=(*lista)->nodo->repeticiones)
+    {
+        nuevo->next= (*lista);
+        (*lista) = nuevo;
+        return;
+    }
+    while ((*lista)->next)
+    {
+        if ((*lista)->next->nodo->repeticiones>=dato)
+        {
+            nuevo->next=(*lista)->next;
+            (*lista)->next=nuevo;
+            (*lista)=inicio;
+            return;
+        }
+        (*lista)=(*lista)->next;
+    }
+    nuevo->next=NULL;
+    (*lista)->next=nuevo;
+    (*lista)=inicio;
 }
 
-//Funcion para guardar los datos ordenados en un archivo
+void mostrarLista (struct list *lista)
+{
+  while (lista)
+    {
+      printf ("\n%c\t%i", lista->nodo->caracter,lista->nodo->repeticiones);
+      lista = lista->next;
+    }
+}
+
+
 
 
 int main(int argc, char *argv[])
 {
     FILE *archivo_entrada;
+    struct list *lista=NULL;
     char *A;
     int tam_archivo;
     int repeticiones[256]={0};                                           
-    archivo_entrada = fopen("prueba", "rb"); //prueba: nombre del archivo con extension
+    archivo_entrada = fopen("prueba", "rb"); //prueba: nombre del archivo con extension ej: michis.mp4
     fseek(archivo_entrada, 0L, SEEK_END);
     tam_archivo=ftell(archivo_entrada);
-    A=(char *)malloc(sizeof(char) * tam_archivo);
+    A=(char *)malloc(sizeof(char) * tam_archivo); //No quitar, se rompe :c
     fseek(archivo_entrada, 0L, SEEK_SET);
     while (!feof(archivo_entrada))
     {
         ++repeticiones[fgetc(archivo_entrada)];
     }
-    // for ( int i = 0; i <= tam_archivo; i++)
-    // {
-    //     ++repeticiones[fgetc(archivo_entrada)];
-    // }
-    
     for (int j = 0; j < 255; j++)
     {
-        if(repeticiones[j]!=0)printf("\n%c\t%i",(char)j,repeticiones[j]);
+        if(repeticiones[j]!=0){
+            insertarLista(&lista,j,repeticiones[j]);
+        }
     }
+    mostrarLista(lista);
     printf("\n\n\t%d\n",tam_archivo);
     
     fclose(archivo_entrada); //Cerramos el archivo de los datos
